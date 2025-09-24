44f7098c4205fdbd79418d37018a64d6ac627456
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-24 12:11:40 +0200

    Updated email to be username or email

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,21 +4,20 @@ import type {FormSubmitEvent} from '@nuxt/ui'
 import {reactive} from "vue";
 
 const schema = z.object({
-  email: z.email('Invalid email'),
-  password: z.string().min(8, 'Must be at least 8 characters')
+  username: z.string(),
+  password: z.string()
 })
 
 type Schema = z.output<typeof schema>
 
 const state = reactive<Partial<Schema>>({
-  email: undefined,
+  username: undefined,
   password: undefined
 })
 
 const toast = useToast()
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  console.dir(state)
   toast.add({title: 'Success', description: 'The form has been submitted.', color: 'success'})
   console.log(event.data)
 }
@@ -28,8 +27,8 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   <div class="flex justify-center">
     <UCard variant="subtle">
       <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-        <UFormField label="Email" name="email">
-          <UInput v-model="state.email"/>
+        <UFormField label="Username/Email" name="username">
+          <UInput v-model="state.username"/>
         </UFormField>
 
         <UFormField label="Password" name="password">
