1c39ef0dc13eb691bbe6e169cba3fe549b24b4e6
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-25 09:31:44 +0200

    Add NextUi auth component to Register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,7 @@
 ﻿<script setup lang="ts">
 import * as z from 'zod'
 import type {FormSubmitEvent} from '@nuxt/ui'
-import {reactive} from "vue";
+import {reactive, ref} from "vue";
 import {useAuthStore} from '@/stores/auth';
 import {useRouter} from 'vue-router';
 import type {AxiosError} from 'axios';
@@ -11,10 +11,10 @@ function isAxiosError(error: unknown): error is AxiosError {
 }
 
 const schema = z.object({
-  username: z.string().trim().nonempty("Username cannot be empty"),
-  email: z.email().trim().nonempty("Email cannot be empty"),
-  password: z.string().trim().nonempty("Password cannot be empty"),
-  password_confirmation: z.string().trim().nonempty("Password confirm cannot be empty"),
+  username: z.string("Username cannot be empty").trim().nonempty("Username cannot be empty"),
+  email: z.email("Email must be a valid email-address").trim().nonempty("Email cannot be empty"),
+  password: z.string("Password cannot be empty").trim().nonempty("Password cannot be empty"),
+  password_confirmation: z.string("Confirm password cannot be empty").trim().nonempty("Password confirm cannot be empty"),
 }).refine((data) => data.password === data.password_confirmation, {
   message: 'Passwords do not match',
   path: ["password_confirmation"],
@@ -22,67 +22,103 @@ const schema = z.object({
 
 type Schema = z.output<typeof schema>
 
-const state = reactive<Partial<Schema>>({
-  username: "",
-  email: "",
-  password: "",
-  password_confirmation: "",
-})
+const providers = [{
+  label: 'Google',
+  icon: 'logos:google-icon',
+  onClick: () => {
+    toast.add({title: 'Google', icon: "logos:google-icon", description: 'Login with Google'})
+  }
+}, {
+  label: 'GitHub',
+  icon: 'lucide:github',
+  onClick: () => {
+    toast.add({title: 'GitHub', icon: "lucide:github", description: 'Login with GitHub'})
+  }
+}]
+
+const fields = [{
+  name: 'username',
+  type: 'text' as const,
+  label: 'Username',
+  placeholder: 'Enter your username',
+  required: true,
+}, {
+  name: 'email',
+  type: 'text' as const,
+  label: 'Email',
+  placeholder: 'Enter your email',
+  required: true,
+}, {
+  name: 'password',
+  label: 'Password',
+  type: 'password' as const,
+  placeholder: 'Enter your password',
+  required: true,
+}, {
+  name: 'password_confirmation',
+  label: 'Password confirmation',
+  type: 'password' as const,
+  placeholder: 'Enter your password again',
+  required: true,
+}]
 
 const toast = useToast()
 const authStore = useAuthStore()
 const router = useRouter()
 
+const errorBox = ref<string | null>(null);
+
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
     await authStore.register(event.data)
-    toast.add({title: 'Registry Successful!', icon: "lucide:user-round-check", description: `Welcome ${authStore.user?.username}`, color: 'success'})
+    toast.add({
+      title: 'Registration Successful!',
+      icon: "lucide:user-round-check",
+      description: `Welcome ${authStore.user?.username}`,
+      color: 'success'
+    })
     await router.push({name: 'home'})
   } catch (error) {
+    let errorMessage = `Unexpected error occurred: ${error}`;
     if (isAxiosError(error) && error.response?.data) {
-      toast.add({title: 'Registry Failed', icon: "lucide:user-x", description: `Error: ${error.response.data}`, color: 'error'})
+      errorMessage = error.response.data as string;
     } else {
-      toast.add({title: 'Registry Failed', icon: "lucide:user-x", description: `An unexpected error occurred: ${error}`, color: 'error'})
+      if (isAxiosError(error) && !error.response) {
+        errorMessage = 'Server is unavailable. Please try again later.';
+      }
     }
+
+    toast.add({
+      title: 'Registration Failed',
+      icon: "lucide:user-x",
+      description: errorMessage,
+      color: 'error'
+    })
+    errorBox.value = errorMessage;
   }
 }
 </script>
 
 <template>
-  <div class="flex justify-center mt-5">
-    <UCard variant="subtle">
-      <template #header>
-        <h2 class="text-2xl text-primary-800">Register</h2>
-      </template>
-      <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-        <UFormField label="Username" name="username">
-          <UInput v-model="state.username" class="w-80"/>
-        </UFormField>
-
-        <UFormField label="Email" name="email">
-          <UInput v-model="state.email" class="w-80"/>
-        </UFormField>
-
-        <UFormField label="Password" name="password">
-          <PasswordField v-model="state.password" class="w-80"></PasswordField>
-        </UFormField>
-
-        <UFormField label="Confirm Password" name="password_confirmation">
-          <PasswordField v-model="state.password_confirmation" class="w-80"></PasswordField>
-        </UFormField>
-
-        <UButton type="submit">
-          Submit
-        </UButton>
-      </UForm>
-
-      <template #footer>
-        <p class="text-center">
+  <div class="flex flex-col items-center justify-center gap-4 p-4">
+    <UPageCard class="w-full max-w-md">
+      <UAuthForm
+          :schema="schema"
+          :fields="fields"
+          :providers="providers"
+          title="Welcome back!"
+          icon="i-lucide-lock"
+          @submit="onSubmit"
+      >
+        <template #description>
           Already have an account?
-          <ULink to="/login" class="text-primary-500 hover:text-secondary-500">Login here
-          </ULink>
-        </p>
-      </template>
-    </UCard>
+          <ULink to="login" class="text-primary font-medium">Login</ULink>
+          .
+        </template>
+        <template #validation>
+          <UAlert v-if="errorBox" color="error" icon="i-lucide-info" :title="errorBox"/>
+        </template>
+      </UAuthForm>
+    </UPageCard>
   </div>
 </template>
