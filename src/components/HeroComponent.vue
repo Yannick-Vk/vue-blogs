b52e32e672bb81b0bf24b9c0347a1c671dbcc773
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-25 16:15:28 +0200

    Added a hero component to the home page

---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,26 @@
+﻿<script setup lang="ts">
+import {ref} from "vue";
+
+const links = ref([
+  {
+    label: 'Create an account',
+    to: '/Login',
+    icon: 'lucide:user'
+  },
+  {
+    label: 'Learn more',
+    to: 'https://github.com/Yannick-Vk/vue-blogs',
+    color: 'neutral',
+    variant: 'subtle',
+    trailingIcon: 'lucide:github'
+  }
+])
+</script>
+
+<template>
+  <UPageHero
+      title="Vue blogger"
+      description="A blogging site made with Vue and Nuxt Ui on the frontend and with a C# web API on the backend, click on any blog to start reading. Want to create your own blogs? Press the login button to create a free account."
+      :links="links"
+  />
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@ import {onMounted} from "vue";
 import {useAuthStore} from "@/stores/auth.ts";
 import {useBlogStore} from "@/stores/blogStore.ts";
 import {storeToRefs} from "pinia";
+import HeroComponent from "@/components/HeroComponent.vue";
 
 const {user} = storeToRefs(useAuthStore());
 const blogStore = useBlogStore();
@@ -15,10 +16,7 @@ onMounted(() => {
 
 <template>
   <main>
-    <UButton class="my-3">
-      <span v-if="user">Welcome {{ user.username }}!</span>
-      <span v-else>Welcome!</span>
-    </UButton>
+    <HeroComponent />
 
     <blog-list :blogs="blogs"/>
   </main>
