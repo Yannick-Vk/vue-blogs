31428617bac5e0257bfc0374a7c9653f52330d5e
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-26 08:54:26 +0200

    Added skeleton and loading icon

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -3,6 +3,7 @@ import type {Blog} from "@/stores/blogStore.ts";
 
 interface Props {
   blogs?: Blog[];
+  loading: boolean;
 }
 
 defineProps<Props>();
@@ -18,8 +19,22 @@ defineProps<Props>();
                     :to="{ name: 'blog-detail', params: { id: blog.id } }"
     />
   </UPageList>
-  <div v-else class="my-3 justify-center text-center text-gray-500 text-2xl">
-    Blogs are loading...
+  <div v-else>
+    <div class="flex justify-center py-4">
+      <UIcon name="lucide:loader-circle" class="size-8 animate-spin"></UIcon>
+    </div>
+    <div v-for="i in 3" :key="i" class="my-3 flex gap-4 items-center">
+      <USkeleton class="h-32 w-48" />
+      <div class="space-y-2 flex-1 py-1">
+        <USkeleton class="h-6 w-3/4" />
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-5/6" />
+        <div class="flex items-center gap-2 pt-4">
+          <USkeleton class="h-8 w-8 rounded-full" />
+          <USkeleton class="h-4 w-1/4" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
