2ca0090f44859e6c90cc54b8d55f7e9b803f0f4e
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-26 08:33:31 +0200

    Added loading text when blogs are still loading

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,13 +1,15 @@
 ﻿<script setup lang="ts">
 import type {Blog} from "@/stores/blogStore.ts";
 
-const props = defineProps({
-  blogs: Array<Blog>,
-})
+interface Props {
+  blogs?: Blog[];
+}
+
+defineProps<Props>();
 </script>
 
 <template>
-  <UPageList divide>
+  <UPageList divide v-if="blogs && blogs.length > 0">
     <blog-list-item class="my-3" v-for="blog in blogs" :key="blog.id"
                     :title="blog.title"
                     :description="blog.description"
@@ -16,6 +18,9 @@ const props = defineProps({
                     :to="{ name: 'blog-detail', params: { id: blog.id } }"
     />
   </UPageList>
+  <div v-else class="my-3 justify-center text-center text-gray-500 text-2xl">
+    Blogs are loading...
+  </div>
 </template>
 
 <style scoped>
