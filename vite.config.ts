e093088d2bb63f2ac2f43c1e11d54c22e057fe99
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-29 09:12:01 +0200

    Updated theme

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
             ui: {
                 colors: {
                     primary: 'fuchsia',
-                    neutral: 'zinc'
+                    neutral: 'slate'
                 }
             }
         }),
