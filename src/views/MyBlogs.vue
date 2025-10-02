5a5ad1310ed5a66e518fdaa5c149b5e3929cbaf9
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-02 12:33:02 +0200

    Added search with submit

--- src/views/MyBlogs.vue
+++ src/views/MyBlogs.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import {useBlogStore} from "@/stores/blogStore.ts";
 import {storeToRefs} from "pinia";
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import BlogLoadingSkeleton from "@/components/BlogLoadingSkeleton.vue";
 
 const blogStore = useBlogStore();
 const {blogs, loading} = storeToRefs(blogStore);
+const searchTerm = ref<string>("");
 
 onMounted(async () => {
   await blogStore.getBlogsByUser();
 })
+
+function search() {
+  console.log("searching blogs for title containing:", searchTerm.value);
+}
 </script>
 
 <template>
@@ -17,6 +22,32 @@ onMounted(async () => {
     <BlogLoadingSkeleton/>
   </div>
   <div v-else-if="blogs.length > 0">
+    <UCard variant="subtle" class="mb-5 text-center">
+      <h2 class="text-2xl">My blogs</h2>
+      <p>You've created {{blogs.length}} blogs!</p>
+      <UForm @submit="search" >
+        <UInput
+            v-model="searchTerm"
+            placeholder="Type something..."
+            :ui="{ trailing: 'pe-1' }"
+            size="xl"
+            class="mt-5"
+        >
+          <template v-if="searchTerm?.length" #trailing>
+            <UButton
+                color="neutral"
+                variant="link"
+                size="sm"
+                icon="lucide:circle-x"
+                aria-label="Clear input"
+                @click="searchTerm = ''"
+            />
+          </template>
+        </UInput>
+        <UButton type="submit" color="neutral" variant="link">Search</UButton>
+      </UForm>
+
+    </UCard>
     <BlogList :blogs="blogs"/>
   </div>
   <div v-else class="no-blogs-message">
