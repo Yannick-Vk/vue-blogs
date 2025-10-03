bac0a3546adcea0040bea4e48b811690167e686d
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-03 09:05:14 +0200

    Added actions menu for removing role

--- src/views/UserDetailsPage.vue
+++ src/views/UserDetailsPage.vue
@@ -1,14 +1,18 @@
 ﻿<script setup lang="ts">
 import {useRoute} from 'vue-router';
-import {type Role, useUserStore} from "@/stores/userStore.ts";
+import {type Role, type User, useUserStore} from "@/stores/userStore.ts";
 import {storeToRefs} from "pinia";
-import {onMounted, ref} from "vue";
+import {h, onMounted, resolveComponent} from "vue";
 import type {TableColumn} from "@nuxt/ui";
+import type {Row} from "@tanstack/vue-table";
 
 const route = useRoute();
 const userId = route.params.id as string;
 const userStore = useUserStore();
 const {currentUser, roles} = storeToRefs(userStore);
+
+const UButton = resolveComponent('UButton')
+const UDropdownMenu = resolveComponent('UDropdownMenu')
 
 onMounted(async () => {
   await userStore.fetchUser(userId);
@@ -25,8 +29,49 @@ const columns: TableColumn<Role>[] = [
     accessorKey: 'name',
     header: 'Name',
   },
+  {
+    id: 'actions',
+    cell: ({row}) => {
+      return h(
+          'div',
+          {class: 'text-right'},
+          h(
+              UDropdownMenu,
+              {
+                content: {
+                  align: 'end'
+                },
+                items: getRowItems(row),
+                'aria-label': 'Actions dropdown'
+              },
+              () =>
+                  h(UButton, {
+                    icon: 'i-lucide-ellipsis-vertical',
+                    color: 'neutral',
+                    variant: 'ghost',
+                    class: 'ml-auto',
+                    'aria-label': 'Actions dropdown'
+                  })
+          )
+      )
+    }
+  },
 ]
 
+function getRowItems(row: Row<User>) {
+  return [
+    {
+      type: 'label',
+      label: 'Actions'
+    },
+    {
+      label: 'Remove role from user',
+      async onSelect() {
+        console.log("Removing role from user!");
+      }
+    },
+  ]
+}
 </script>
 
 <template>
