67927b481ccc8903bac368c9f754509057997bd8
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-06 14:55:17 +0200

    Added change email form

---
+++ src/stores/profileStore.ts
@@ -0,0 +1,16 @@
+﻿import {defineStore} from "pinia";
+import {api} from "@/services/Api.ts";
+
+export const useProfileStore = defineStore('profile', () => {
+    async function changeEmail(newEmail: string) {
+        try {
+            await api.put(`/me/change/email`, {
+                email: newEmail
+            })
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    return {changeEmail}
+})
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,44 @@
 ﻿<script setup lang="ts">
 import {useAuthStore} from "@/stores/auth.ts";
+import * as z from 'zod'
+import type {FormSubmitEvent} from '@nuxt/ui'
+import {reactive} from "vue";
+import {useProfileStore} from "@/stores/profileStore.ts";
 
 const authStore = useAuthStore();
 const user = authStore.user;
+const profileStore = useProfileStore();
+
+const schema = z.object({
+  email: z.email('Email is required')
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive<Partial<Schema>>({
+  email: undefined
+})
+
+const toast = useToast()
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  try {
+    await profileStore.changeEmail(event.data.email);
+    toast.add({
+      title: 'Successfully changed email',
+      description: `Email has been changed to ${event.data.email}`,
+      color: 'success'
+    })
+  } catch (e) {
+    console.error(e)
+    toast.add({
+      title: "Failed to change email",
+      description: `Failed to update email-address to ${event.data.email}`,
+      color: 'error'
+    })
+  }
+
+}
 </script>
 
 <template>
@@ -18,6 +54,19 @@ const user = authStore.user;
         }"
           size="3xl"
       />
+      <UCard variant="subtle" class="my-4">
+        <h3 class="text-lg mb-3 text-primary">Change email</h3>
+        <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+          <UFormField label="Email" name="email">
+            <UInput v-model="state.email" type="email" placeholder="Please enter new email ..." class="w-96"/>
+          </UFormField>
+
+          <UButton type="submit">
+            Change email
+          </UButton>
+        </UForm>
+      </UCard>
+
 
     </div>
 
