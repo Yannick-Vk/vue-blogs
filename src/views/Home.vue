ccbfe7063b7d6d91547a7678ddaa7124d10425f7
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-06 09:13:13 +0200

    Added searchbar to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {onMounted} from "vue";
+import {computed, onMounted, ref} from "vue";
 import {useAuthStore} from "@/stores/auth.ts";
 import {useBlogStore} from "@/stores/blogStore.ts";
 import {storeToRefs} from "pinia";
@@ -12,10 +12,23 @@ const {blogs, error} = storeToRefs(blogStore);
 onMounted(() => {
   blogStore.getAllBlogs();
 });
+
+const searchTerm = ref<string>("");
+
+const filteredBlogs = computed(() => {
+  const term = searchTerm.value.trim().toLowerCase();
+  if (term.length < 1) {
+    return blogs.value;
+  }
+  return blogs.value.filter((blog) => blog.title.toLowerCase().includes(term));
+});
 </script>
 
 <template>
   <HeroComponent/>
-
-  <blog-list :blogs="blogs" :error="error"/>
+  <UCard variant="subtle" color="primary" class="my-3 flex flex-row gap-3 justify-center">
+    <p class="text-lg mb-3">Search blogs: </p>
+    <SearchBox v-model:search-term="searchTerm" />
+  </UCard>
+  <blog-list :blogs="filteredBlogs" :error="error"/>
 </template>
