3c1a8c150cc96de7e64bbc11cfac71d9a323e004
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-06 13:46:39 +0200

    Show loggedIn user details

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,30 @@
 ﻿<script setup lang="ts">
+import {useAuthStore} from "@/stores/auth.ts";
 
+const authStore = useAuthStore();
+const user = authStore.user;
 </script>
 
 <template>
-  <h2>Profile</h2>
+  <div class="p-4">
+    <h2 class="text-2xl font-bold mb-4">Profile</h2>
+    <div v-if="user">
+      <UUser
+
+          :name="user.username"
+          :description="user.email"
+          :avatar="{
+          src: `https://i.pravatar.cc/64?u=${user.username}`,
+        }"
+          size="3xl"
+      />
+
+    </div>
+
+    <div v-else>
+      <p>Loading user profile...</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
