10e8bb04f27097222475e319b7b5b9d422adf012
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-06 15:57:31 +0200

    Added form field labels to all inputs

--- src/components/ChangeEmailForm.vue
+++ src/components/ChangeEmailForm.vue
@@ -25,12 +25,14 @@ function onSubmit(event: FormSubmitEvent<Schema>) {
 <template>
   <UCard variant="subtle" class="my-4">
     <h3 class="text-lg mb-3 text-primary">Change email</h3>
-    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit" >
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
       <UFormField label="Email" name="email">
         <UInput v-model="state.email" type="email" placeholder="Please enter new email ..." class="w-full"/>
       </UFormField>
 
-      <PasswordField v-model="state.password" class="block w-full"/>
+      <UFormField label="Password" name="password">
+        <PasswordField v-model="state.password" class="block w-full"/>
+      </UFormField>
 
       <UButton type="submit">
         Change email
--- src/components/ChangePasswordForm.vue
+++ src/components/ChangePasswordForm.vue
@@ -25,10 +25,14 @@ function onSubmit(event: FormSubmitEvent<Schema>) {
 <template>
   <UCard variant="subtle" class="my-4">
     <h3 class="text-lg mb-3 text-primary">Change Password</h3>
-    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit" >
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
 
-      <PasswordField v-model="state.newPassword" placeholder="New password..." class="block w-full"/>
-      <PasswordField v-model="state.password" placeholder="Old Password..." class="block w-full"/>
+      <UFormField label="New Password" name="newPassword">
+        <PasswordField v-model="state.newPassword" placeholder="New password..." class="block w-full"/>
+      </UFormField>
+      <UFormField label="Old Password" name="password">
+        <PasswordField v-model="state.password" placeholder="Old Password..." class="block w-full"/>
+      </UFormField>
 
       <UButton type="submit">
         Change email
