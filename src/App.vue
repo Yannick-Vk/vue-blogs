59c54cf76f2aabda0f1de1625843661fd672ee60
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-24 12:49:05 +0200

    Updated toast position

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import {useAuthStore} from '@/stores/auth';
 import type {ToasterProps} from "@nuxt/ui/components/Toaster.vue";
 
 const authStore = useAuthStore();
-const toaster: ToasterProps = {position: 'top-center', expand: false};
+const toaster: ToasterProps = {position: 'bottom-right', expand: false};
 
 onMounted(() => {
   authStore.fetchUser();
