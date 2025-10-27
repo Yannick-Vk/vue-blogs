66b2d8f4ef4574416c9fe1afa6c9c42c608fad1e
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-27 11:51:46 +0100

    Removed Authform

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -3,7 +3,7 @@ import * as z from 'zod'
 import {reactive, ref} from 'vue'
 import type {FormSubmitEvent} from "@nuxt/ui";
 import {useProfileStore} from "@/stores/profileStore.ts";
-import {useRouter} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 
 const schema = z.object({
   password: z.string("Password cannot be empty").trim().min(8, "Password must contain at least 8 characters"),
@@ -16,37 +16,28 @@ const state = reactive<Partial<Schema>>({
   password: undefined,
 })
 
-const fields = [{
-  name: 'password',
-  label: 'Password',
-  type: 'password' as const,
-  placeholder: 'Enter your password',
-  required: true,
-}, {
-  name: 'password_confirmation',
-  label: 'Password confirmation',
-  type: 'password' as const,
-  placeholder: 'Enter your password again',
-  required: true,
-}]
-
 const errorBox = ref<string | null>(null);
 
 const toast = useToast()
 const profileStore = useProfileStore();
 const router = useRouter();
+const route = useRoute();
+const userId = route.path.id as string;
+const token = route.query.token as string;
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
     errorBox.value = null;
-    await profileStore.confirmResetPassword("", "", event.data.password);
+    //await profileStore.confirmResetPassword(userId, token, event.data.password);
+    console.log("userId", userId)
+    console.log("token", token)
     toast.add({
       title: 'Password has been reset',
       description: 'Your password have been reset, please login to continue.',
       color: 'success',
       icon: 'lucide:check-circle',
     })
-    await router.push('/login');
+    //await router.push('/login');
   } catch (e) {
     console.error(e);
     errorBox.value = e;
@@ -57,21 +48,27 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 
 <template>
   <div class="flex flex-col items-center justify-center gap-4 p-4">
+    <div>
+      {{ token }} , {{ userId }}
+    </div>
     <UPageCard class="w-full max-w-md">
       <template #header>
         <h2 class="text-2xl text-primary">Reset password</h2>
       </template>
       <template #body>
-        <UAuthForm :fields="fields" :schema="schema" class="space-y-4" @submit="onSubmit">
+        <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
 
-          <template #validation>
-            <UAlert v-if="errorBox" :title="errorBox" color="error" icon="i-lucide-info"/>
-          </template>
+          <UFormField label="Password" name="newPassword">
+            <PasswordField v-model="state.password" class="block w-full" placeholder="Password..."/>
+          </UFormField>
+          <UFormField label="Confirm Password" name="confirmPassword">
+            <PasswordField v-model="state.confirmPassword" class="block w-full" placeholder="Confirm password..."/>
+          </UFormField>
 
           <UButton type="submit">
             Reset password
           </UButton>
-        </UAuthForm>
+        </UForm>
       </template>
     </UPageCard>
   </div>
